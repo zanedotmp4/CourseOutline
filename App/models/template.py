62bcf5f8819ba4d90e0430f8d8bce9845a56a60f
zanedotmp4 <zaneamour@gmail.com>
2023-02-18 22:11:10 +0000

    more quick fixes

--- App/models/template.py
+++ App/models/template.py
@@ -1,5 +1,5 @@
 from App.database import db
-from App.models import AssigmentTemp,ContentTemp,DescriptionTemp,LearningOutcomesTemp,RationaleTemp,ResourcesTemp,TeachingStratigiesTemp
+from App.models import *
 class template(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     LearningOutcomes = db.Column(db.Integer, db.ForeignKey('LearningOutcomesTemp.id'), nullable=False)
