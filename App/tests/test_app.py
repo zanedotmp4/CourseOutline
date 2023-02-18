f251e16c6065d519e9e259a5ea53abf42d592b4d
zanedotmp4 <zaneamour@gmail.com>
2023-02-18 02:31:38 +0000

    yea imma be honest i dont like this

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -24,24 +24,24 @@ LOGGER = logging.getLogger(__name__)
 class UserUnitTests(unittest.TestCase):
 
     def test_new_user(self):
-        user = User("bob", "bobpass")
+        user = User("bob", "bobpass","bob@mail.com")
         assert user.username == "bob"
 
     # pure function no side effects or integrations called
     def test_toJSON(self):
-        user = User("bob", "bobpass")
+        user = User("bob", "bobpass","bob@mail.com")
         user_json = user.toJSON()
-        self.assertDictEqual(user_json, {"id":None, "username":"bob"})
+        self.assertDictEqual(user_json, {"id":None, "username":"bob","Email":"bob@mail.com"})
     
     def test_hashed_password(self):
         password = "mypass"
         hashed = generate_password_hash(password, method='sha256')
-        user = User("bob", password)
+        user = User("bob", password,"bob@mail.com")
         assert user.password != password
 
     def test_check_password(self):
         password = "mypass"
-        user = User("bob", password)
+        user = User("bob", password,"bob@mail.com")
         assert user.check_password(password)
 
 '''
