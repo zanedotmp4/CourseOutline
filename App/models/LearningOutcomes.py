26a5b4087fe945e760dc73dbcaa83a890aa5810b
zanedotmp4 <zaneamour@gmail.com>
2023-03-21 16:50:26 +0000

    yea idk how that managed to happen lol

--- App/models/LearningOutcomes.py
+++ App/models/LearningOutcomes.py
@@ -4,7 +4,7 @@ class LearningOutcomes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     docId = db.Column(db.Integer, db.ForeignKey('document.id'), nullable=False)
     templateid= db.Column(db.Integer, db.ForeignKey('template.id'), nullable=False)
-    assigmentid = db.Column(db.Integer, db.ForeignKey('Assigment.id'), nullable=False)
+    assigmentid = db.Column(db.Integer, db.ForeignKey('assigment.id'), nullable=False)
     text = db.Column(db.String(120),nullable=False)
     def __init__(self,text):
         self.text = text
--- App/models/document.py
+++ App/models/document.py
@@ -2,12 +2,12 @@ from App.database import db
 from App.models import LearningOutcomes,Assigment,Rationale,Content,Resources,TeachingStratigies,Resources,template
 class document(db.Model):
      id = db.Column(db.Integer, primary_key=True)
-     LearningOutcomes = db.Column(db.Integer, db.ForeignKey('LearningOutcomes.id'), nullable=False)
-     Assigment = db.Column(db.Integer, db.ForeignKey('Assigment.id'), nullable=False)
-     rationale = db.Column(db.Integer, db.ForeignKey('Rationale.id'), nullable=False)
-     content = db.Column(db.Integer, db.ForeignKey('Content.id'), nullable=False)
-     resources = db.Column(db.Integer, db.ForeignKey('Resources.id'), nullable=False)
-     stratigies = db.Column(db.Integer, db.ForeignKey('TeachingStratigies.id'), nullable=False)
+     LearningOutcomes = db.Column(db.Integer, db.ForeignKey('learning_outcomes.id'), nullable=False)
+     Assigment = db.Column(db.Integer, db.ForeignKey('assigment.id'), nullable=False)
+     rationale = db.Column(db.Integer, db.ForeignKey('rationale.id'), nullable=False)
+     content = db.Column(db.Integer, db.ForeignKey('content.id'), nullable=False)
+     resources = db.Column(db.Integer, db.ForeignKey('resources.id'), nullable=False)
+     stratigies = db.Column(db.Integer, db.ForeignKey('teaching_stratigies.id'), nullable=False)
      templateID = db.Column(db.Integer, db.ForeignKey('template.id'), nullable=False)
      name  = db.Column(db.String(120),nullable=False)
 
