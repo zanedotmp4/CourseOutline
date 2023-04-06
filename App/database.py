846357bfc9b96547f0c57094346c0949c180aab2
basstech <basstech@sundaebytes.com>
2023-04-06 15:44:42 +0000

    deployment is deployment

--- App/database.py
+++ App/database.py
@@ -7,8 +7,7 @@ def get_migrate(app):
     return Migrate(app, db)
 
 def create_db(app):
-    db.init_app(app)
-    db.create_all(app=app)
+    db.create_all()
     
 def init_db(app):
     db.init_app(app)
--- App/main.py
+++ App/main.py
@@ -7,52 +7,41 @@ from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 from datetime import timedelta
 
-
-from App.database import create_db
+from App.database import init_db
+from App.config import config
 
 from App.controllers import (
-    setup_jwt
+    setup_jwt,
+    setup_flask_login
 )
 
 from App.views import views
-
 
 def add_views(app):
     for view in views:
         app.register_blueprint(view)
 
-
-def loadConfig(app, config):
-    app.config['ENV'] = os.environ.get('ENV', 'DEVELOPMENT')
-    delta = 7
-    if app.config['ENV'] == "DEVELOPMENT":
-        app.config.from_object('App.config')
-        delta = app.config['JWT_EXPIRATION_DELTA']
-    else:
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
-        app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-        app.config['DEBUG'] = os.environ.get('ENV').upper() != 'PRODUCTION'
-        app.config['ENV'] = os.environ.get('ENV')
-        delta = os.environ.get('JWT_EXPIRATION_DELTA', 7)
-        
-    app.config['JWT_EXPIRATION_DELTA'] = timedelta(days=int(delta))
-        
+def configure_app(app, config, overrides):
     for key, value in config.items():
-        app.config[key] = config[key]
+        if key in overrides:
+            app.config[key] = overrides[key]
+        else:
+            app.config[key] = config[key]
 
-def create_app(config={}):
+def create_app(config_overrides={}):
     app = Flask(__name__, static_url_path='/static')
-    CORS(app)
-    loadConfig(app, config)
+    configure_app(app, config, config_overrides)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['SEVER_NAME'] = '0.0.0.0'
     app.config['PREFERRED_URL_SCHEME'] = 'https'
     app.config['UPLOADED_PHOTOS_DEST'] = "App/uploads"
+    CORS(app)
     photos = UploadSet('photos', TEXT + DOCUMENTS + IMAGES)
     configure_uploads(app, photos)
     add_views(app)
-    create_db(app)
+    init_db(app)
     setup_jwt(app)
+    setup_flask_login(app)
     app.app_context().push()
     return app
