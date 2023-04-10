fd0196aee595573f0c4856ca93240a8b9ba3c1a4
zanedotmp4 <zaneamour@gmail.com>
2023-04-10 14:38:29 +0000

    quick change

--- App/views/form.py
+++ App/views/form.py
@@ -1,17 +1,19 @@
 from flask import Flask, request, jsonify
 from App.database import db
 from App.models import document
+from maker.py import *
 
 
 app = Flask(__name__)
 
-@app.route('/App/views/form.py', methods=['POST'])
+@app.route('/App/views/form', methods=['POST'])
 def handle_form_data():
     data = request.get_json()
     function_name = data.get('function_name')
   
     if function_name == "submitForm":
-        doc = document(binary_data=data)
+        formdata = writeDoc(data)
+        doc = document(binary_data=formdata)
         db.session.add(doc)
         db.session.commit()
 
