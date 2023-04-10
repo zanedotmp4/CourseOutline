76ab7d5a5dfd8bb72dfe671405d0042e291f4ab5
zanedotmp4 <zaneamour@gmail.com>
2023-04-10 14:45:24 +0000

    fixed the user.py for vie

--- App/views/user.py
+++ App/views/user.py
@@ -1,16 +1,18 @@
 from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
-from flask_jwt import jwt_required, current_identity
+from flask_jwt_extended import jwt_required, current_user as jwt_current_user
+from flask_login import current_user, login_required
 
 from.index import index_views
 
 from App.controllers import (
-    create_user, 
+    create_user,
+    jwt_authenticate, 
     get_all_users,
     get_all_users_json,
+    jwt_required
 )
 
 user_views = Blueprint('user_views', __name__, template_folder='../templates')
-
 
 @user_views.route('/users', methods=['GET'])
 def get_user_page():
@@ -34,13 +36,6 @@ def create_user_action():
     flash(f"User {data['username']} created!")
     create_user(data['username'], data['password'])
     return redirect(url_for('user_views.get_user_page'))
-    
-
-
-@user_views.route('/identify', methods=['GET'])
-@jwt_required()
-def identify_user_action():
-    return jsonify({'message': f"username: {current_identity.username}, id : {current_identity.id}"})
 
 @user_views.route('/static/users', methods=['GET'])
 def static_user_page():
