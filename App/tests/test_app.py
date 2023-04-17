e5f02232b9563c1967f7cc4f4a2b58dda1555097
zanedotmp4 <zaneamour@gmail.com>
2023-04-17 16:13:16 +0000

    yea lol

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -3,7 +3,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from App.main import create_app
 from App.database import db, create_db
-from App.models import User
+from App.models import *
 from App.controllers import (
     create_user,
     get_all_users_json,
